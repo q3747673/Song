cda184210b3a1f1a0471ae6a9cd023a697324ac4
hi <gg0880933@gmail.com>
2023-05-18 17:03:19 +0800

    first commit

---
+++ src/main/java/com/example/Song/SongApplication.java
@@ -0,0 +1,13 @@
+package com.example.Song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Song/SongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
